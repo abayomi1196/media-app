d1854ae9a8a5740415e7cfd55cbcef7bfad9189d
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-27 06:53:46 +0100

    delete comment functionality

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-import { UserInputError } from "apollo-server";
+import { AuthenticationError, UserInputError } from "apollo-server";
 
 import Post from "../../models/Post.js";
 import checkAuth from "../../utils/checkAuth.js";
@@ -30,6 +30,27 @@ export default {
         return post;
       } else {
         throw new UserInputError("Post not found!");
+      }
+    },
+
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
       }
     },
   },
