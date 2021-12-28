636fdc3203e67727e9582e003aef4d2c88005ac3
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-28 07:08:53 +0100

    finalize server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -28,6 +28,10 @@ export default {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty!!");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
