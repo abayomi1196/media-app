c1dad42d4f94afa839a40111e81cc2981a5c57d4
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-28 07:32:21 +0100

    fix mongo auth issue

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,7 +4,7 @@ import commentsResolvers from "./comments.js";
 import likesResolvers from "./likes.js";
 
 export default {
-  Post: {
+  SinglePost: {
     likeCount: (parent) => parent.likes.length,
     commentCount: (parent) => parent.comments.length,
   },
