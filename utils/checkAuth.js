d44e0fd0c3839bb61a3a803451509a5fcdf92c3a
Abayomi Ayodele <samabayomi301@gmail.com>
2022-01-02 17:47:20 +0100

    update start command

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import { AuthenticationError } from "apollo-server";
 
+import { SECRET_KEY } from "../config.js";
+
 export default function (context) {
   // context = {..., req: {headers}}
   const authHeader = context.req.headers.authorization;
