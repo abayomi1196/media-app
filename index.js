e2c062fcc62252dbe0d813d8e9f728ab8e05fda8
Abayomi Ayodele <samabayomi301@gmail.com>
2022-01-02 17:53:04 +0100

    fix node start & port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { MONGODB } from "./config.js";
 import { typeDefs } from "./graphql/types.js";
 import resolvers from "./graphql/resolvers/index.js";
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 // instantiate server instance
 const server = new ApolloServer({
